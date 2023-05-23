0119e681a948b02ee13baa3a828891fbb57882c5
Rcong <zcr0903@gmail.com>
2023-05-23 23:59:11 +0800

    some draft

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,16 +4,54 @@ import (
 	"log"
 	"mangosteen/internal/database"
 	"mangosteen/internal/router"
+
+	"github.com/spf13/cobra"
 )
 
+func Run() {
+	rootCmd := &cobra.Command{
+		Use: "mangosteen",
+	}
+	srvCmd := &cobra.Command{
+		Use: "server",
+		Run: func(cmd *cobra.Command, args []string) {
+			RunServer()
+		},
+	}
+	dbCmd := &cobra.Command{
+		Use: "db",
+	}
+	createCmd := &cobra.Command{
+		Use: "create",
+		Run: func(cmd *cobra.Command, args []string) {
+			database.PgCreateTables()
+		},
+	}
+	mgrtCmd := &cobra.Command{
+		Use: "migrate",
+		Run: func(cmd *cobra.Command, args []string) {
+			database.Migrate()
+		},
+	}
+	crudCmd := &cobra.Command{
+		Use: "crud",
+		Run: func(cmd *cobra.Command, args []string) {
+			database.Crud()
+		},
+	}
+	database.PgConnect()
+	rootCmd.AddCommand(srvCmd, dbCmd)
+	dbCmd.AddCommand(createCmd, mgrtCmd, crudCmd)
+	defer database.PgClose()
+	rootCmd.Execute()
+}
+
 func RunServer() {
-	database.Connect()
-	database.CreateTables()
-	defer database.Close()
 	r := router.New()
 	err := r.Run(":8080")
 	if err != nil {
 		log.Fatalln(err)
 	}
 	log.Println("r.Run 的下一行")
+
 }
--- internal/database/pg.go
+++ internal/database/pg.go
@@ -16,7 +16,7 @@ const (
 	dbname   = "mangosteen_dev"
 )
 
-func Connect() {
+func PgConnect() {
 	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 	db, err := sql.Open("postgres", connStr)
@@ -31,7 +31,7 @@ func Connect() {
 	log.Println("Successfully connect to db")
 }
 
-func CreateTables() {
+func PgCreateTables() {
 	// 创建 users 表
 	_, err := DB.Exec(`CREATE TABLE IF NOT EXISTS users (
 		id SERIAL PRIMARY KEY,
@@ -44,8 +44,79 @@ func CreateTables() {
 	}
 	log.Println("Successfully create users table")
 }
+func Migrate() {
+	// 给user添加手机字段
+	_, err := DB.Exec(`ALTER TABLE users ADD COLUMN phone VARCHAR(50)`)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("Successfully add phone column to users table")
+	}
 
-func Close() {
+	_, err = DB.Exec(`ALTER TABLE users ADD COLUMN address VARCHAR(200)`)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("Successfully add address column to users table")
+	}
+
+	// 新增 Items 表，字段为 id, amount, happened_at, created_at, updated_at
+	_, err = DB.Exec(`
+	CREATE TABLE items(
+		id SERIAL PRIMARY KEY,
+		amount INTEGER NOT NULL,
+		happened_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+ );
+	`)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("Successfully create items table")
+	}
+
+	_, err = DB.Exec(`ALTER TABLE items ALTER COLUMN happened_at TYPE TIMESTAMP`)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("Successfully change the type of happened_at to TIMESTAMP")
+	}
+}
+
+func Crud() {
+	// 创建一个 User
+	_, err := DB.Exec(`INSERT INTO users (email) values ('2@qq.com')`)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("Successfully create a user")
+	}
+	_, err = DB.Exec(`Update users SET phone = 138123456789 where email = '2@qq.com'`)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("Successfully update a user")
+	}
+	stmt, err := DB.Prepare("SELECT phone FROM users where email = $1 offset $2 limit $3")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	result, err := stmt.Query("2@qq.com", 0, 3)
+	if err != nil {
+		log.Println(err)
+	} else {
+		for result.Next() {
+			var phone string
+			result.Scan(&phone)
+			log.Println("phone", phone)
+		}
+		log.Println("Successfully read users")
+	}
+
+}
+
+func PgClose() {
 	DB.Close()
 	log.Println("Successfully close db")
 }
